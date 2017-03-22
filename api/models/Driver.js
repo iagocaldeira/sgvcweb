55c7333b12c0f1a34aba40a63b9a5687a649b6c8
Igor Caldeira <igorcaldeiraa@gmail.com>
2017-03-22 10:42:44 -0300

    initial commit

---
+++ api/models/Driver.js
@@ -0,0 +1,24 @@
+/**
+ * Driver.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    name : { type: 'string' },
+
+    plate : { type: 'string' },
+
+    city : { type: 'string' },
+
+    trackActions : {
+    	collection: 'trackAction',
+    	via: 'driver'
+    }
+  }
+};
+
---
+++ api/models/TrackAction.js
@@ -0,0 +1,23 @@
+/**
+ * TrackAction.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    
+    position : { type: 'string' },
+
+    time : { type: 'string' },
+    
+    isStart : { type: 'boolean' },
+    
+    isEnd : { type: 'boolean' },
+
+    driver : {  model: 'driver'}
+  }
+};
+
